cdd32dccdee396f5c2c3f8bcf6bb8b4fa4313c31
Gunnar Russell <russell.gunnar@gmail.com>
2024-04-12 11:01:38 -0400

    added data validation

--- index.js
+++ index.js
@@ -8,35 +8,81 @@ let num = 1;
 
 button.addEventListener("click", function()
 {
-    //create new row and cells
-    var newRow = document.createElement("tr");
-    var cellNum = document.createElement("th");
-    var cellFirstName = document.createElement("td");
-    var cellLastName = document.createElement("td");
-    var cellEmail = document.createElement("td");
-
     //data validation
-    cellNum.innerHTML = num++;
-    cellFirstName.innerHTML = inputFirstName.value;
-    cellLastName.innerHTML = inputLastName.value;
-    cellEmail.innerHTML = inputEmail.value;
-
-    //append new row to myTable
-    myTable.appendChild(newRow);
-    newRow.appendChild(cellNum);
-    newRow.appendChild(cellFirstName);
-    newRow.appendChild(cellLastName);
-    newRow.appendChild(cellEmail);
-
-    //clear input fields
-    //grab all input fields and put them into array
-    let inputFields = Array.from(document.getElementsByClassName("input-fields"));
-    //for each input field grabbed above:
-    inputFields.forEach(function(inputField)
+    if(inputFirstName.value == "") //if first name field is empty
     {
-        //clear text
-        inputField.value = "";
-    });
+        //undo any style changes
+        inputFirstName.style.borderColor = "";
+        inputLastName.style.borderColor = "";
+        inputEmail.style.borderColor = "";
+
+        //highlight input field
+        inputFirstName.style.borderColor = 'red';
+        alert("You must enter a first name!");
+    }
+    else if(inputLastName.value == "") //if last name field is empty
+    {
+        //undo any style changes
+        inputFirstName.style.borderColor = "";
+        inputLastName.style.borderColor = "";
+        inputEmail.style.borderColor = "";
+
+        //highlight input field
+        inputLastName.style.borderColor = 'red';
+        alert("You must enter a last name!");
+    }
+    else if(inputEmail.value == "") //if email field is empty
+    {
+        //undo any style changes
+        inputFirstName.style.borderColor = "";
+        inputLastName.style.borderColor = "";
+        inputEmail.style.borderColor = "";
+
+        //highlight input field
+        inputEmail.style.borderColor = 'red';
+        alert("You must enter an email!");
+    }
+    else
+    {
+        //undo any style changes
+        inputFirstName.style.borderColor = "";
+        inputLastName.style.borderColor = "";
+        inputEmail.style.borderColor = "";
+
+        //proceed as planned
+
+        //create new row and cells
+        var newRow = document.createElement("tr");
+        var cellNum = document.createElement("th");
+        var cellFirstName = document.createElement("td");
+        var cellLastName = document.createElement("td");
+        var cellEmail = document.createElement("td");
+        
+        //set cell data to input field data
+        cellNum.innerHTML = num++;
+        cellFirstName.innerHTML = inputFirstName.value;
+        cellLastName.innerHTML = inputLastName.value;
+        cellEmail.innerHTML = inputEmail.value;
+
+        //append new row to myTable
+        myTable.appendChild(newRow);
+        newRow.appendChild(cellNum);
+        newRow.appendChild(cellFirstName);
+        newRow.appendChild(cellLastName);
+        newRow.appendChild(cellEmail);
+
+        //clear input fields
+        //grab all input fields and put them into array
+        let inputFields = Array.from(document.getElementsByClassName("input-fields"));
+        //for each input field grabbed above:
+        inputFields.forEach(function(inputField)
+        {
+            //clear text
+            inputField.value = "";
+        });
+    }
+
+    
 
 
     
